68cfaff2c40b1cff02948c7a20b6da7a48625b49
epl <me@example.com>
2022-12-19 13:48:39 +0100

    auto

--- runkirs.py
+++ runkirs.py
@@ -1,5 +1,5 @@
 import math,sys
 if __name__=='__main__':
-  y=math.pi*math.log2(16)
-  b=str(y)+str(math.tan(len('maqok')))
-  print(b+'gayagow')
+  y=math.pi*math.log2(31)
+  b=str(y)+str(math.tan(len('gesay')))
+  print(b+'qiweme')
