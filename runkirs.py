df0724fda259f4c0d51e17fe2b4feb8dc285a3b5
epl <me@example.com>
2023-01-02 18:25:28 +0100

    auto

--- runkirs.py
+++ runkirs.py
@@ -1,5 +1,5 @@
 import math,sys
 if __name__=='__main__':
-  y=math.pi*math.log2(35)
-  b=str(y)+str(math.tan(len('tumosaw')))
-  print(b+'nabafavij')
+  y=math.pi*math.log2(53)
+  b=str(y)+str(math.tan(len('retamiv')))
+  print(b+'renami')
